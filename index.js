9f744c854ccd2f3ee9348893a64e819eccfac71f
JuanSerranoP <jserpit@gmail.com>
2023-08-27 11:42:57 +0200

    a

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ const fs = require('fs');
 
 const app = express();
 const port = 3001;
-app.use(express.json());
+
 const options = {
     key: fs.readFileSync('./certificates/key.pem'),
     cert: fs.readFileSync('./certificates/cert.pem')
 }
 
-
-// CORS
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 // });
+app.use(express.json());
 
 app.use("/users", Users);
 
@@ -31,6 +30,7 @@ main()
     .then(() => console.log('Estamos conectados a la DB'))
     .catch(err => console.log(err));
 
-https.createServer(options, (req, res) => {
-    ;
-}).listen(port, () => { console.log(`Backend de SharingMe emitiendo por el puerto ${port}`) });
+https.createServer(options)
+app.listen(port, () => {
+    console.log(`Backend de SharingMe emitiendo por el puerto ${port}`)
+});
