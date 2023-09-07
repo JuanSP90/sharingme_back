c69f5e48b2fbeecb1acd97d21ecc385113f74768
JuanSerranoP <jserpit@gmail.com>
2023-09-07 17:10:59 +0200

    mejoras

--- mailConfig.js
+++ mailConfig.js
@@ -5,11 +5,11 @@ const transporter = nodemailer.createTransport({
     // host: "smtp-mail.outlook.com",
     // port: 587,
     // secure: false,
-    service: 'Hotmail',
-    auth: {
-        user: 'sharingmeapp@hotmail.com',
-        pass: process.env.EMAILPASSWORD
-    }
+    // service: 'Hotmail',
+    // auth: {
+    //     user: 'sharingmeapp@hotmail.com',
+    //     pass: process.env.EMAILPASSWORD
+    // }
 });
 
 module.exports = transporter;
